2b63374dc7db477c853c18c12be34e0ed001b433
20IT153 <20it153@charusat.edu.in>
2021-10-08 17:53:47 +0530

    commit

---
+++ adminpanel.Designer.cs
@@ -0,0 +1,47 @@
+﻿
+namespace thalbhet
+{
+    partial class adminpanel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // adminpanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1464, 669);
+            this.Name = "adminpanel";
+            this.Text = "adminpanel";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ whatsapptest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using RestSharp;
+using RestSharp.Authenticators;
+
+namespace thalbhet
+{
+    public partial class whatsapptest : Form
+    {
+        public whatsapptest()
+        {
+            InitializeComponent();
+        }
+        String username = "kundal";
+        String password = "Kundal212#";
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //using (WebClient web = new WebClient())
+            //{
+            //    string url = string.Format("https://wa.krupacc.com/send-message")
+            //}
+            //HttpClient Client = new HttpClient();
+            //StringContent Content = new StringContent("");
+            //await Client.PostAsync("https://wa.krupacc.com/send-message", Content);
+        }
+    }
+}
